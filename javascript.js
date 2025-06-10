772d40d327633edd838d0020439144b9c4b0a9f2
Julian-Bazo <Julian.Bazo27@gmail.com>
2025-06-10 08:41:31 -0400

    Initialize main math function "operate"
    Creates the operate function that determines which basic math function will called depending on the "operator" variable.

--- javascript.js
+++ javascript.js
@@ -20,3 +20,24 @@ function division(num1, num2) {
     let quotient = num1 / num2;
     return quotient;
 }
+
+// Initialization of Operator function variables with filler values
+let firstNum = 5;
+let secondNum = 3;
+let operator = `+`;
+
+// Operate function
+function operate(firstNum, operator, secondNum) {
+    if (operator === "+") {
+        return addition(firstNum, secondNum);
+    }
+    if (operator === "-") {
+        return subtraction(firstNum, secondNum);
+    }
+    if (operator === "*" || operator === "x") {
+        return multiplication(firstNum, secondNum);
+    }
+    if (operator === "/") {
+        return division(firstNum, secondNum);
+    }
+}
