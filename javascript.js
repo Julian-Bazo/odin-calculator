020f7ee0acb12dfa072b4064677019aa1e2a9b23
Julian-Bazo <Julian.Bazo27@gmail.com>
2025-06-10 08:27:31 -0400

    Create functions for basic math operations
    Adds 4 functions, addition, subtraction, multiplication, and division.

---
+++ javascript.js
@@ -0,0 +1,22 @@
+// Math function initialization
+
+// Addition function
+function addition(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+}
+// Subtraction function
+function subtraction(num1, num2) {
+    let difference = num1 - num2;
+    return difference;
+}
+// Multiplication function
+function multiplication(num1, num2) {
+    let product = num1 * num2;
+    return product;
+}
+// Division function
+function division(num1, num2) {
+    let quotient = num1 / num2;
+    return quotient;
+}
